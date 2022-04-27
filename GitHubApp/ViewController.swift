80af4ad04c7e1984cf92bf8ba0801f453e36880a
Tiago <tiagosdeanna@gmail.com>
2022-04-27 09:55:01 -0300

    Adicionar swiftlint

--- GitHubApp/ViewController.swift
+++ GitHubApp/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  GitHubApp
-//
-//  Created by Tiago Deanna on 26/04/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
     }
-
-
 }
-
---
+++ Podfile
@@ -0,0 +1,11 @@
+# source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '13.0'
+inhibit_all_warnings!
+
+target 'GitHubApp' do  
+  use_frameworks!
+
+  pod 'SwiftLint'
+
+end
